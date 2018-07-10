bd7ca9314a319d22f39068a863532e07f831761e
jetpan <jetpan2000@yahoo.ca>
2018-07-09 23:56:17 -0400

    angular6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { EmployeeComponent } from './employee/employee.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, EmployeeComponent
   ],
   imports: [
     BrowserModule
