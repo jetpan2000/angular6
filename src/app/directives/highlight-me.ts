0882cbb75c1a8f5bc30bba9cd453246f74679075
jetpan <jetpan2000@yahoo.ca>
2018-07-18 08:53:18 -0400

    added customized directive

--- src/app/directives/highlight-me.ts
+++ src/app/directives/highlight-me.ts
@@ -1,11 +1,27 @@
-import {Directive, ElementRef} from "@angular/core";
+import {Directive, ElementRef, HostListener} from "@angular/core";
 
 @Directive({
     selector: '[highlight-me]'
 })
 
 export class MyDirectives{
-    constructor(el: ElementRef){
-        el.nativeElement.style.backgroundColor = 'blue';
+    constructor(private el: ElementRef){
+        this.el.nativeElement.style.backgroundColor = 'blue';
+    }
+
+    @HostListener('mouseenter')
+    onmouseenter(): void{
+        this.pleaseHighLight('yellow');
+    }
+
+    @HostListener('mouseleave')
+    onmouseleave(){
+        this.pleaseHighLight('white');
+    }
+
+    private pleaseHighLight(color:string){
+        this.el.nativeElement.style.backgroundColor = color;
+    }
+
     }
 }
